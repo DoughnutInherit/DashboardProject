021623ffe26bcf19011105c61947991353d0eb99
Jan Vidal Fonseca <jvidal@pasiona.es>
2019-02-14 12:11:54 +0100

    Componente tiempo 2

--- my-app/src/services/serviceWorker.js
+++ my-app/src/services/serviceWorker.js
@@ -1,11 +1,9 @@
 import Request from 'superagent';
 
-const getWeatherFromUrl = (url) => new Promise(((resolve, reject) => {
+export const getWeatherFromUrl = (url) => new Promise(((resolve, reject) => {
   Request('GET', url)
     .end((err, res) => {
       if (err) reject(err);
       resolve(res.body);
     });
 }));
-
-export default getWeatherFromUrl;
