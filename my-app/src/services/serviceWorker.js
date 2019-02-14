6e079c8f2c0d06113c36a68e748349cf0d0c7cd3
nachocebey <n.cebey@gmail.com>
2019-02-14 11:22:32 +0100

    Add json

--- my-app/src/services/serviceWorker.js
+++ my-app/src/services/serviceWorker.js
@@ -1,9 +1,11 @@
 import Request from 'superagent';
 
-export const getWeatherFromUrl = (url) => new Promise(((resolve, reject) => {
+const getWeatherFromUrl = (url) => new Promise(((resolve, reject) => {
   Request('GET', url)
     .end((err, res) => {
       if (err) reject(err);
       resolve(res.body);
     });
 }));
+
+export default getWeatherFromUrl;
