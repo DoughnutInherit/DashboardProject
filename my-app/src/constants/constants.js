343b10056ed432e04f72955e52f72f67c11957e5
Jan Vidal Fonseca <jvidal@pasiona.es>
2019-02-19 12:28:10 +0100

    Icons are functional

---
+++ my-app/src/constants/constants.js
@@ -0,0 +1,5 @@
+export const months = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'] ;
+
+export const weatherUrl = () => 'https://api.openweathermap.org/data/2.5/weather?q=barcelona&units=metric&appid=c51a88807c66237f50daf2bda22a8a10';
+
+export const video = 'https://www.w3schools.com/tags/movie.mp4';
