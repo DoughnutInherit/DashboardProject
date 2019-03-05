a8c340c6f03eefc36bc2bf078c82f83269ef843d
Amin Chouaibi El Azaar <achouaibi@pasiona.es>
2019-03-05 12:42:12 +0100

    Saving changes

---
+++ DashboardProjectBackEnd/DasboardProjectBE/DasboardProjectBE.Domain.Entities/ApplicationUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DasboardProjectBE.ServiceLibrary.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [MaxLength(200)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string LastName { get; set; }
+    }
+}
---
+++ DashboardProjectBackEnd/DasboardProjectBE/DasboardProjectBE/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DasboardProjectBE.ServiceLibrary.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DasboardProjectBE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        readonly UserManager<ApplicationUser> userManager;
+
+    }
+}
