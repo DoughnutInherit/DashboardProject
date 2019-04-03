8d0989a28670e0ffd062b406a7f0e7ce1520851c
Jan Vidal Fonseca <jvidal@pasiona.es>
2019-04-03 11:05:53 +0200

    backofficedone6.0

--- my-app/src/containers/BackOffice/BackOffice.jsx
+++ my-app/src/containers/BackOffice/BackOffice.jsx
@@ -1,21 +1,33 @@
 /* eslint-disable react/no-unknown-property */
 import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import FormBackOffice from '../../components/BackOffice/BackOffice';
 import { postBackOffice } from '../../services/serviceWorker';
 
 
 class BackOffice extends Component {
+  static propTypes = {
+    bearerToken: PropTypes.object,
+    history: PropTypes.object,
+  }
+
+  navigate = () => {
+    this.props.history.push('DashBoard');
+  }
 
   submitEvent = (object) => {
 
     const entryDate = object.date + ' ' + object.iniHour;
-    const departureDate = object.date + ' ' + object.iniHour;
+    const departureDate = object.date + ' ' + object.endHour;
     const type = "1";
 
     object = { ...object, entryDate, departureDate, typeid: type }
+    const eo = "Bearer " + this.props.bearerToken;
     debugger;
 
-    postBackOffice('https://localhost:5001/api/event/', object, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJheWxsb25AZ21haWwuY29tIiwianRpIjoiZmRhMDMzNzAtNzUxNi00NjMxLWJmNjktZWY2MDg4YTQwMTQxIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZWlkZW50aWZpZXIiOiJhODU1NjFkOC0wMTkxLTQ1NzAtYmViZi0wMGVmMDBkNGEyZWIiLCJleHAiOjE1NTM1MTQ1ODIsImlzcyI6Imh0dHA6Ly95b3VyZG9tYWluLmNvbSIsImF1ZCI6Imh0dHA6Ly95b3VyZG9tYWluLmNvbSJ9.Q8bg5XrJWM3S5Z0wDhKrtdurm7xxOAHcOCcUBa3fMMo')
+    postBackOffice('https://localhost:5001/api/event/', object, eo)
+    .then(x=>this.navigate())
   };
 
   render() {
@@ -29,4 +41,12 @@ class BackOffice extends Component {
   }
 }
 
-export default BackOffice;
+const mapStateToProps = (state) => ({
+  events: state.appointment.events,
+  index: state.appointment.eventIndex,
+  event: state.appointment.event,
+  bearerToken: state.loginReducer.bearerToken,
+});
+
+export default connect(mapStateToProps, {
+})(BackOffice);
--- my-app/src/services/serviceWorker.js
+++ my-app/src/services/serviceWorker.js
@@ -1,4 +1,5 @@
 import Request from 'superagent';
+import moment from 'moment';
 
 export const getWeatherFromUrl = (url) => new Promise(((resolve, reject) => {
   Request('GET', url)
@@ -9,6 +10,11 @@ export const getWeatherFromUrl = (url) => new Promise(((resolve, reject) => {
       }
     });
 }));
+
+export function calculateDifference(firstTime, secondTime) {
+  const result = firstTime.diff(secondTime);
+  return result;
+}
 
 export const getDailyEvents = (url) => new Promise(((resolve, reject) => {
   Request('GET', url)
@@ -21,12 +27,12 @@ export const getDailyEvents = (url) => new Promise(((resolve, reject) => {
 export const postBackOffice = (url, info, token) => new Promise(((resolve, reject) => {
   Request('POST', url)
     .send(info)
-    .set('authorization', 'Bearer ' + token)
-    .set('Content-Type','application/json')
+    .set('authorization', token)
+    .set('Content-Type', 'application/json')
     .end((err, res) => {
       if (err) reject(err);
       else
-      resolve(res.body);
+        resolve(res.body);
     });
 }));
 
@@ -40,6 +46,75 @@ export function compare(a, b) {
   return 0;
 }
 
+function nextEvent(title, length, index) {
+  if (title !== undefined) {
+    if (index === length) {
+      return 0;
+    }
+    if (index === 0) {
+      return 1;
+    }
+  }
+  return index;
+}
+
+export function calculateTotalEventTime(index, events) {
+  const startEventTime = moment(events[index].entryDate);
+  const endEventTime = moment(events[index].departureDate);
+  const totalTime = calculateDifference(endEventTime, startEventTime);
+  return totalTime;
+}
+
+export function calculateUntilEventEnd(events, index, title) {
+  const indexAux = nextEvent(title, events.length, index);
+  const now = moment();
+  const actionTime = moment(events[indexAux].departureDate);
+  const timeRemeaning = calculateDifference(actionTime, now);
+  return timeRemeaning;
+}
+
+
+export function checkNextActionTime(index, title, events) {
+  const now = moment();
+  let check;
+  const indexAux = nextEvent(title, events.length, index);
+  if (index === 0 && title !== undefined) {
+    check = false;
+  } else {
+    let eventTimeIni;
+    if (indexAux === 0 && title !== undefined) {
+      check = true;
+    } else {
+      try {
+        eventTimeIni = moment(events[indexAux].entryDate);
+        const timeRemeaning = calculateDifference(eventTimeIni, now);
+        if (timeRemeaning <= 0) {
+          check = true;
+        } else {
+          check = false;
+        }
+      } catch (error) {
+        return false;
+      }
+    }
+  }
+  return check;
+}
+
+export function setCheckedIndex(index, setIndex) {
+  if (index === 0) {
+    setIndex(1);
+  }
+}
+
+export function calculateUntilEventStart(events, index, title, allDayEvent, setEvent) {
+  const now = moment();
+  const indexAux = nextEvent(title, events.length, index);
+  setEvent(allDayEvent);
+  const actionTime = moment(events[indexAux].entryDate);
+  const timeRemeaning = calculateDifference(actionTime, now);
+  return timeRemeaning;
+}
 export const getJwtBearer = (user, method, url) => new Promise(((resolve, reject) => {
   debugger;
   Request(method, url)
@@ -47,6 +122,7 @@ export const getJwtBearer = (user, method, url) => new Promise(((resolve, reject
     .set('email', user.email)
     .set('password', user.password)
     .end((err, result) => {
+      debugger;
       if (err) reject(err);
       resolve(result);
     });
