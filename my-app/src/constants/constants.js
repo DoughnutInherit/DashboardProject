32c94315268e5c2caef750e4c5996ffa38d857de
Nacho Cebey <ncebey@pasiona.com>
2019-07-09 15:09:02 +0200

    delte files

--- my-app/src/constants/constants.js
+++
@@ -1,7 +0,0 @@
-export const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-
-export const weatherUrl = () => 'https://api.openweathermap.org/data/2.5/weather?q=barcelona&units=metric&appid=c51a88807c66237f50daf2bda22a8a10';
-
-export const backgroundFoto = 'https://images.pexels.com/photos/1432942/pexels-photo-1432942.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
-
-export const getLoginControllerUrl = () => 'https://localhost:5001/api/account/login';
